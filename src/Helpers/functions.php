73df338ba36ac039aba7107b6b39d595a0a0a8a9
Jhinwy <1257015959@qq.com>
2021-06-28 14:28:48 +0800

    init

---
+++ src/Helpers/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: Jhinwy
+ * Date: 6/28/21
+ * Time: 2:26 PM
+ */
---
+++ src/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: Jhinwy
+ * Date: 6/28/21
+ * Time: 2:27 PM
+ */
+
+namespace Jhin\Lib;
+
+class Test
+{
+    public function test()
+    {
+        echo 'Hello Jhin-Lib';
+    }
+}
